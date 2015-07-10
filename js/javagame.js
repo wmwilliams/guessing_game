2228bc6da2d3774073b3ea0832bddb5a29795789
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-10 13:22:08 -0700

    Split HTML and JS files, trying to add DOM

---
+++ js/javagame.js
@@ -0,0 +1,20 @@
+//  var Game = function() {
+//   var userGuess;
+//   this.answer = 24;
+//   this.guess = function() {
+//     userGuess = prompt("See if you can guess how old I am but be careful you can only have 2 guesses....");
+//   }
+//   this.response = function() {
+//     if(userGuess != this.answer){
+//     userGuess = prompt("Nice try, but try again")
+//     }if(userGuess == this.answer){
+//     alert("Wow good job!");
+//     }
+//   }
+// }
+// var game = new Game();
+// game.guess();
+// game.response();
+// alert("Game Over!")
+var list1 = document.getElementsByClassName("hot");
+
