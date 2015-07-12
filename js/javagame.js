a287b288e9408b331521542a019ff721bad35d41
Wyatt Williams <williams.wyatt@gmail.com>
2015-07-12 16:41:32 -0700

    Added a basic DOM manipulation

--- js/javagame.js
+++ js/javagame.js
@@ -1,20 +1,17 @@
-//  var Game = function() {
-//   var userGuess;
-//   this.answer = 24;
-//   this.guess = function() {
-//     userGuess = prompt("See if you can guess how old I am but be careful you can only have 2 guesses....");
-//   }
-//   this.response = function() {
-//     if(userGuess != this.answer){
-//     userGuess = prompt("Nice try, but try again")
-//     }if(userGuess == this.answer){
-//     alert("Wow good job!");
-//     }
-//   }
-// }
-// var game = new Game();
-// game.guess();
-// game.response();
-// alert("Game Over!")
-var list1 = document.getElementsByClassName("hot");
-
+ var Game = function() {
+  var userGuess;
+  this.answer = 24;
+  this.guess = function() {
+    userGuess = prompt("See if you can guess how old I am but be careful you can only have 2 guesses....");
+  }
+  this.response = function() {
+    if(userGuess != this.answer){
+    userGuess = prompt("Nice try, but try again")
+    }if(userGuess == this.answer){
+    document.getElementById('title').textContent = "Good job, you got it!";
+    }
+  }
+ }
+var game = new Game();
+game.guess();
+game.response();
